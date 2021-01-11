61deb347aa03ec2ebb3c9137f643bf507435ed52
Derraphics <derrickmuriithi988@gmail.com>
2021-01-11 17:40:57 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Pizza Campany
+#### This is not the first website that I have made 
+#### Writen By Derrick Muriithi W
+## Description
+   
+
+## Setup/Installation Requirements
+* Clone the repository to your local machine
+    ```
+    $ git clone https://github.com/Derraphics/Pizza-.git
+    ```
+* Move into the cloned directory
+    ```
+    $ cd Pizza
+    ```
+* Open the project on VS Code and liveserver
+## Known Bugs
+  Currently the project is working as expected
+## Technologies Used
+* HTML
+* css
+* javascript
+* Jquery
+## Support and contact details
+If your would like to contribute to this project, fork the project and raise a PR
+### License
+[MIT](https://choosealicense.com/licenses/mit/)
+Copyright (c) 2020 Derrick Muriithi w
---
+++ java.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $("#order").click(function(){
+      $("#form1").toggle();
+    });
+  });
+  
+  function Results (type,size,crust,toppings) {
+    this.type = type;
+    this.size= size;
+    this.crust= crust;
+    this.toppings=toppings;
+  };
+  Results.prototype.order = function() {
+    return "You have made an order of " + this.type + " pizza  with " + this.toppings + " as toppings and " + this.crust + " for crust ."
+  };
+  function TotalPrice (price, quantity, delivery,toppings,crust) {
+    this.price= price;
+    this.quantity=quantity;
+    this.delivery=delivery;
+    this.toppings=toppings;
+    this.crust=crust;
+  };
+  TotalPrice.prototype.finalTotal = function () {
+    return ( this.price + this.delivery + this.toppings + this.crust )* this.quantity ;
+  };
+  
+  let sizePrices = [1200, 800, 600]
+  let priceToppings = [100,120,80,150,200]
+  let toppingsName= ["Pepperoni" , "mushroom" , "salad" ,"sausage", " salami"]
+  let crustNames= ["Crispy", "Stuffed", "Glutton-free"]
+  let crustPrices = [100,120,200]
+  let deliveryPrices = [0, 200];
+  $(document).ready(function(){
+    $('form#form1').submit(function (event) {
+        event.preventDefault();
+    let pizzaType = $('#type').val();
+    let pizzaSize = parseInt($('#size').val());
+    let pizzaToppings=parseInt($('#toppings').val());
+    let priceCrust =parseInt($('#crust').val());
+    let pizzaTop = $('#toppings').val();
+    let pizzaQty = parseInt($('#quantity').val());
+    let pizzaPick = parseInt($('#delivery').val());
+    let price = sizePrices[pizzaSize - 1];
+    let DeliveryCost = deliveryPrices[pizzaPick - 1];
+    let ToppingsCost = priceToppings[pizzaToppings-1];
+    let crustCost = crustPrices[priceCrust-1]
+    let topNames = toppingsName[pizzaTop-1]
+    let crustName = crustNames[priceCrust-1] 
+    newOrder = new Results(pizzaType,pizzaSize, crustName,topNames,crustName);
+    newTotal = new TotalPrice(price, pizzaQty, DeliveryCost,ToppingsCost,crustCost);
+    if (pizzaPick===1){
+        alert( newOrder.order());
+        alert("Your bill is: " + newTotal.finalTotal());
+        alert("Thank you for shopping with us! " )
+        }else{
+            if(pizzaPick===2){
+               let location= prompt(" Where would your like your Pizza to be Delivered? ");
+               alert("Your pizza will be delivered at " + location + " in a few minutes from now and you will be charged $20 for delivery");
+               alert(newOrder.order());
+               alert("Your bill is: " + newTotal.finalTotal());
+               alert("Thank you for shopping with us!")
+            }
+        }
+  })
+  });
+  function validateForm() {
+    let name = document.getElementById("name").value;
+    alert( name + " we have received your message thank you for your feedback");
+  }
+  
